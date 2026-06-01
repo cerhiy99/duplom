5b57b3381e5f52777c874300a673fd7f5344eb9b
cerhiy99 <cerhiy99@gmail.com>
2026-06-01 19:27:44 +0300

    fix bag 2

--- server/Controller/TempEmail.js
+++ server/Controller/TempEmail.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('../db.js');
-const { Email } = require('../models/models.js');
+const { Email, Letter } = require('../models/models.js');
 
 class TempEmail {
   static Create = async (req, res, next) => {
@@ -64,7 +64,7 @@ class TempEmail {
   static GetLetters=async(req,res,next)=>{
     try{
         const {accessKey}=req.query;
-        const result=await Email.findAll({where:{accessKey}});
+        const result=await Letter.findAll({where:{accessKey}});
         return res.json({data:result});
     }catch(err){
         console.log("Помилка отримання листів", err);
